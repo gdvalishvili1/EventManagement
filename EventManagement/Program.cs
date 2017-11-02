245f82535c00b566815fc11d52b71786100331a3
ggdvalishvili <ggdvalishvili@gmail.com>
2017-11-02 18:40:36 +0400

    INIT

--- EventManagement/Program.cs
+++ EventManagement/Program.cs
@@ -6,7 +6,7 @@ namespace EventManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello WorldAAA!");
         }
     }
 }

f85a684c2112251514973f87afd672713d2e2286
ggdvalishvili <ggdvalishvili@gmail.com>
2017-12-28 10:06:24 +0400

    moved EfRepository to separate file

--- Domain/EventManagementContext/EventManagement.Infrastructure/Persistence/EFConcertRepository.cs
+++ Domain/EventManagementContext/EventManagement.Infrastructure/Persistence/EFConcertRepository.cs
@@ -5,40 +5,6 @@ using System.Linq;
 
 namespace EventManagement.Infrastructure.Persistence
 {
-    public class EFRepository<TAggregate, TDataModel, Tsnapshot> : IRepository<TAggregate>
-        where TAggregate : AggregateRoot, IVersionedAggregateRoot, IProvideSnapshot<Tsnapshot>
-        where Tsnapshot : class, new()
-        where TDataModel : class, Idbentity
-    {
-        private readonly IAggregateFactory<TAggregate, Tsnapshot> _aggregateFactory;
-        private readonly EventContext _db;
-        public EFRepository(EventContext db, IAggregateFactory<TAggregate, Tsnapshot> aggregateFactory)
-        {
-            _db = db;
-            _aggregateFactory = aggregateFactory;
-        }
-        public TAggregate OfId(string id)
-        {
-            var entity = _db.Set<TDataModel>().FirstOrDefault(x => x.Id == Guid.Parse(id));
-            //return Tagg
-            return _aggregateFactory.CreateFrom(new Tsnapshot());
-        }
-
-        public void Delete(TAggregate aggregateRoot)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Store(TAggregate aggregateRoot)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(TAggregate aggregateRoot)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public class EFConcertRepository : IConcertRepository, IDisposable
     {
         private readonly EventContext _db;
---
+++ Domain/EventManagementContext/EventManagement.Infrastructure/Persistence/EFRepository.cs
@@ -0,0 +1,44 @@
+﻿using EventManagement.ConcertAggregate;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventManagement.Infrastructure.Persistence
+{
+    public class EFRepository<TAggregate, TDataModel, Tsnapshot> : IRepository<TAggregate>
+        where TAggregate : AggregateRoot, IVersionedAggregateRoot, IProvideSnapshot<Tsnapshot>
+        where Tsnapshot : class, new()
+        where TDataModel : class, Idbentity
+    {
+        private readonly IAggregateFactory<TAggregate, Tsnapshot> _aggregateFactory;
+        private readonly EventContext _db;
+        public EFRepository(EventContext db, IAggregateFactory<TAggregate, Tsnapshot> aggregateFactory)
+        {
+            _db = db;
+            _aggregateFactory = aggregateFactory;
+        }
+        public TAggregate OfId(string id)
+        {
+            var entity = _db.Set<TDataModel>().FirstOrDefault(x => x.Id == Guid.Parse(id));
+            //return Tagg
+            return _aggregateFactory.CreateFrom(new Tsnapshot());
+        }
+
+        public void Delete(TAggregate aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store(TAggregate aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TAggregate aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
